bdc1c969d79e5f9c8d239e1b1c5b5982e9fbf8c0
Syed Haider <shayder426@gmail.com>
2020-09-03 00:53:48 -0700

    Initial commit

---
+++ src/main/java/com/spring/autospotify/AutoSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.autospotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/autospotify/AutoSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.autospotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
